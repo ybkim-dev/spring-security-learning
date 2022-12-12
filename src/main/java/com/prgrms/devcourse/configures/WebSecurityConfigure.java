20ee97b23e1fd92ac2927f80af6bc9a19042d041
ybkim-dev <beanybigggy@gmail.com>
2022-12-12 16:39:03 +0900

    init project
    Spring Security 기본 설정으로 프로젝트 시작

---
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -0,0 +1,31 @@
+package com.prgrms.devcourse.configures;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/assets/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .anyRequest().permitAll();
+        http.formLogin()
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+        ;
+    }
+
+}
